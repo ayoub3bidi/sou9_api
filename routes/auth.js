e6d837a53774fa9566433e3551efd21395d28bb7
ayoub3bidi <mrayoubabidi@gmail.com>
2022-01-24 12:25:00 +0100

    ✅ Adding JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -24,20 +25,28 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post('/login', async (req, res) => {
     try{
-        const user = await User.findOne({userName: req.body.user_name});
+        const user = await User.findOne({userName: req.body.username});
         !user && res.status(401).json("Wrong User Name");
 
         const hashedPassword = CryptoJS.AES.decrypt(
             user.password,
             process.env.PASS_SEC
         );
-
         const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
         const inputPassword = req.body.password;
         originalPassword != inputPassword && res.status(401).json("Wrong Password");
 
+        const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+
         const { password, ...others } = user._doc;  
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
 
     } catch(err){
         res.status(500).json(err);
