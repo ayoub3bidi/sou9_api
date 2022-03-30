1259ab8356b8c6c406a9c79c987a0d35f47505d6
ayoub3bidi <mrayoubabidi@gmail.com>
2022-03-30 16:39:30 +0100

    ✅ Adding CORS and stripeRoute

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors")
 
 app.use(express.json());
 dotenv.config();
@@ -17,12 +19,13 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+app.use(cors())
 app.use("/auth", authRoute);
 app.use("/users", userRoute);
 app.use("/products", productRoute);
 app.use("/carts", cartRoute);
 app.use("/orders", orderRoute);
+app.use("/checkout", stripeRoute);
 
   app.listen( process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
